a7f75bec19dacc97f99d747d4ca1aec3e1cc4526
Daniel Villalobos <Daniel.villalobosdel@gmail.com>
2017-02-07 19:02:26 +0100

    Upload all files of first practise
    All files of the first practise

---
+++ practica1/main.c
@@ -0,0 +1,65 @@
+#include <lpc17xx.h>
+#include <math.h>					
+#define NUM_MAX 65535				
+
+int primo[65535];						
+int limite = 0;							
+uint16_t numeroEvaluado = 3;			
+int pos = 0;	
+
+
+int main()								
+	{
+		int i;											
+		int esPrimo = 1;						
+		primo[0] = 2;						
+		while (numeroEvaluado < NUM_MAX){	
+			limite = sqrt(numeroEvaluado)+1;		
+			for(i=1; i<=pos; i++){									
+				if (primo[i] > limite)								
+					break;
+				if(numeroEvaluado%primo[i] == 0){			
+					esPrimo = 0;												
+					break;
+				}				
+			}																			
+			if (esPrimo == 1){									 
+				pos++;														
+				primo[pos] = numeroEvaluado;	
+			}
+			esPrimo = 1;												
+			numeroEvaluado+=2;									
+		}																	
+	}
+	
+/*
+uint8_t esprimo(uint16_t numero){ 
+	int i; 
+limite = sqrt(numeroEvaluado)+1;		
+			for(i=1; i<=pos; i++){									
+				if (primo[i] > limite)								
+				return 1;
+				if(numeroEvaluado%primo[i] == 0){			
+					return 0;										
+				}				
+		}
+			return 1;
+}
+
+int main (){
+
+	uint8_t ifprimo=1;
+	primo[0]=2;
+	while (numeroEvaluado < NUM_MAX)
+	{
+		ifprimo = esprimo(numeroEvaluado);
+		if(ifprimo == 1){
+			pos++;														
+				primo[pos] = numeroEvaluado;	
+		}
+		ifprimo=1;
+		numeroEvaluado +=2;
+	
+	}
+}
+*/
